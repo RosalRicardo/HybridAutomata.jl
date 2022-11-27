23023176a4169c56204b0d96120ed658577ccc50
rosalricardo <ricardo.rosal@uni9.edu.br>
2022-11-26 22:14:06 -0300

    definicao do struct e funcao de criacao de arestras

--- src/HybridAutomata.jl
+++ src/HybridAutomata.jl
@@ -1,5 +1,24 @@
 module HybridAutomata
 
 # Write your package code here.
+    Base.@kwdef mutable struct Automata
+        discrete_states::Dict{Any,Any} = Dict()
+        continuos_states::Dict{Any,Any} = Dict()
+        guard_map::Vector{Function} = []
+        reset_map::Vector{Function} = []
+    end
 
+    function make_edges(automata::Automata)
+        edges = []
+        estados_discretos = keys(automata.discrete_states)
+        for i ∈ estados_discretos
+            for j ∈ estados_discretos
+                push!(edges,i => j)
+            end
+        end
+        return edges
+    end
+
+    export make_edges
 end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using HybridAutomata
 using Test
 
+estados = Dict("primeira marcha"=>1,
+"segunda marcha"=>2,
+"terceira marcha"=>3)
+
+motor = HybridAutomata.Automata(discrete_states=estados)
+
+HybridAutomata.make_edges(motor)
+
 @testset "HybridAutomata.jl" begin
     # Write your tests here.
+ 
 end
