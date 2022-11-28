490eecaf81903809f873025162ebe7a3784e94b8
rosalricardo <ricardo.rosal@uni9.edu.br>
2022-11-27 21:52:39 -0300

    definir automato estados guarda e flow

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HybridAutomata
 
 [![Build Status](https://github.com/RosalRicardo/HybridAutomata.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/RosalRicardo/HybridAutomata.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+## Estados discretos
--- src/HybridAutomata.jl
+++ src/HybridAutomata.jl
@@ -4,21 +4,53 @@ module HybridAutomata
     Base.@kwdef mutable struct Automata
         discrete_states::Dict{Any,Any} = Dict()
         continuos_states::Dict{Any,Any} = Dict()
-        guard_map::Vector{Function} = []
+        edges::Vector{Pair} = []
+        guard_map::Matrix{Any} = Array{Any}(undef, 0, 0)
+        flow_map::Matrix{Any} = Array{Any}(undef, 0, 0)
         reset_map::Vector{Function} = []
     end
 
-    function make_edges(automata::Automata)
+    function Automata(discrete_states::Dict{Any,Any},continuos_states::Dict{Any,Any})
+        println(discrete_states)
+        println(continuos_states)
         edges = []
-        estados_discretos = keys(automata.discrete_states)
+        estados_discretos = keys(discrete_states)
         for i ∈ estados_discretos
             for j ∈ estados_discretos
                 push!(edges,i => j)
             end
         end
+        guard_map = zeros(size(discrete_states)[1]^2,size(discrete_states)[1]^2)
+        new(discrete_states,continuos_states,edges,guard_map)
+    end
+
+    function make_edges(automata::Automata)
+        edges = []
+        estados_discretos = collect(keys(automata.discrete_states))
+        for i ∈ estados_discretos
+            for j ∈ estados_discretos
+                push!(edges,i => j)
+            end
+        end
+        guard_map = zeros(size(edges)[1])
+        automata.edges = edges
+        automata.guard_map = hcat(edges,guard_map)
+        automata.flow_map = hcat(estados_discretos,zeros(size(estados_discretos)[1]))
         return edges
     end
 
+    function add_guard(automato::Automata,edge::Pair,guard::Function)
+        idx = findfirst(==(edge),automato.guard_map[:,1])
+        automato.guard_map[idx,2] = guard
+    end
+
+    function add_flow(automato::Automata,mode::Int,flow::Function)
+        idx = findfirst(==(mode),automato.flow_map[:,1])
+        automato.guard_map[idx,2] = flow
+    end
+
     export make_edges
+    export add_guard
+    export add_flow
 end
 
